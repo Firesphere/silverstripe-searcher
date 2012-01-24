eb9b9716692a7475bc0662cb9d68fadeef469269
Simon Erkelens <simon@squal.nl>
2012-01-24 23:39:58 +0100

    Languages English and Dutch

--- lang/en_US.php
+++ lang/en_US.php
@@ -2,3 +2,25 @@
 
 global $lang;
 
+$lang['en_US']['SearchReport']['CREATED'] = 'Search time/date';
+$lang['en_US']['SearchReport']['DESCRIPTION'] = 'Show searchqueries';
+$lang['en_US']['SearchReport']['FROM_URL'] = 'Search requested from this URL';
+$lang['en_US']['SearchReport']['QUERY'] = 'Searchquery';
+$lang['en_US']['SearchReport']['TITLE'] = 'Searchreport';
+
+$lang['en_US']['SearchResultsPage.ss']['NO_SEARCH_RESULTS'] = 'Sorry, your search did not return any results.';
+$lang['en_US']['SearchResultsPage.ss']['READ_MORE'] = 'Read more';
+$lang['en_US']['SearchResultsPage.ss']['YOU_SEARCHED_FOR'] = 'You searched for';
+
+$lang['en_US']['SearcherDecorator']['KEYWORDS'] = 'Search enhancement Keywords';
+$lang['en_US']['SearcherDecorator']['SEARCHER_INTRO'] = 'Specify keywords for the sitesearch';
+$lang['en_US']['SearcherDecorator']['TAB_SEARCHER'] = 'Search';
+
+$lang['en_US']['TopSearchesReport']['ALL'] = 'All';
+$lang['en_US']['TopSearchesReport']['DESCRIPTION'] = 'Show top searchquery\'s';
+$lang['en_US']['TopSearchesReport']['LAST7DAYS'] = 'Last 7 days';
+$lang['en_US']['TopSearchesReport']['OCCURENCES'] = 'Occurences';
+$lang['en_US']['TopSearchesReport']['PERIOD'] = 'Period';
+$lang['en_US']['TopSearchesReport']['QUERY'] = 'Searchquery';
+$lang['en_US']['TopSearchesReport']['TITLE'] = 'Top searches report';
+
--- lang/nl_NL.php
+++ lang/nl_NL.php
@@ -2,3 +2,25 @@
 
 global $lang;
 
+$lang['nl_NL']['SearchReport']['CREATED'] = 'Zoeken op datum/tijd';
+$lang['nl_NL']['SearchReport']['DESCRIPTION'] = 'Toon zoekopdrachten';
+$lang['nl_NL']['SearchReport']['FROM_URL'] = 'Zoekopdrachten vanaf deze URL';
+$lang['nl_NL']['SearchReport']['QUERY'] = 'Zoekopdracht';
+$lang['nl_NL']['SearchReport']['TITLE'] = 'Zoekrapportage';
+
+$lang['nl_NL']['SearchResultsPage.ss']['NO_SEARCH_RESULTS'] = 'Sorry, je zoekopdracht leverde geen resultaten';
+$lang['nl_NL']['SearchResultsPage.ss']['READ_MORE'] = 'Lees meer';
+$lang['nl_NL']['SearchResultsPage.ss']['YOU_SEARCHED_FOR'] = 'Je hebt gezocht op';
+
+$lang['nl_NL']['SearcherDecorator']['KEYWORDS'] = 'Keywords voor zoeken';
+$lang['nl_NL']['SearcherDecorator']['SEARCHER_INTRO'] = 'Specificeer keywords voor het doorzoeken van de site';
+$lang['nl_NL']['SearcherDecorator']['TAB_SEARCHER'] = 'Zoeken';
+
+$lang['nl_NL']['TopSearchesReport']['ALL'] = 'Alles';
+$lang['nl_NL']['TopSearchesReport']['DESCRIPTION'] = 'Toon de meest uitgevoerde zoekopdrachten';
+$lang['nl_NL']['TopSearchesReport']['LAST7DAYS'] = 'Laatste 7 dagen';
+$lang['nl_NL']['TopSearchesReport']['OCCURENCES'] = 'Aantal keren';
+$lang['nl_NL']['TopSearchesReport']['PERIOD'] = 'Periode';
+$lang['nl_NL']['TopSearchesReport']['QUERY'] = 'Zoekopdracht';
+$lang['nl_NL']['TopSearchesReport']['TITLE'] = 'Meest uitgevoerde zoekopdrachten';
+
