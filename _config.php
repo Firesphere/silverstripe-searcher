4f8f0335aeebb8fff3b0d05400efae2700d70b35
Simon Erkelens <simon@squal.nl>
2012-01-26 18:20:53 +0100

    Hahahaha! Did I forget to add the report?

--- _config.php
+++ _config.php
@@ -1,6 +1,11 @@
 <?php 
-/*
+/**
  * Add extensions to SiteTree. The FulltextSearchable is 1 field more then the default, hence the extra declaration
  */
 Object::add_extension('SiteTree', 'SearcherDecorator');
 Object::add_extension('SiteTree', "FulltextSearchable('Title,SearchKeywords,MenuTitle,Content,MetaTitle,MetaDescription,MetaKeywords')");
+/**
+ * Add search-reports to the reportadmin
+ */
+SS_Report::register('ReportAdmin', 'SearchReport', -12);
+SS_Report::register('ReportAdmin', 'TopSearchesReport', -11);
